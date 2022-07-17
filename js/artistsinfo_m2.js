df4ee4d727d118324eaab23ee0e24bdc41b06356
Yuto Hayashi <hyasssy@gmail.com>
2022-07-17 17:59:08 +0900

    m2情報流し込み

--- js/artistsinfo_m2.js
+++ js/artistsinfo_m2.js
@@ -9,30 +9,35 @@ const _m2data = [
             name_ja: "今宮 有葉",
             name_en: "Ariha Imamiya",
             work1_img: "./images/artistsinfo/m2/imamiya/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《色彩楽譜》（2022）",
             work2_img: "./images/artistsinfo/m2/imamiya/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《Dear.chopin》（2021）",
             work3_img: "",
             work3_caption: "",
             info: `プロフィール
 1996年東京都出身。共感覚の共有をテーマに、視覚と聴覚相互の情報の変換を可能とする楽器と記譜法(ノーテーション)を研究、絵画と様々なメディアによる制作を行っている。
 東京藝術大学美術学部先端芸術表現科卒業。
 
+展示歴
 2021「Dimentions」(Whitestone Gallery新館)
 2021「アートのかけら番外編‐さくらマーケット」(藝大アートプラザ）
 2021 「令和2年度東京藝術大学卒業／修了制作展」（東京都美術館）
 2021 「第15回アートプラザ大賞展」(藝大アートプラザ）
 2020「がんばれ藝大生「エールのアトリエ」(藝大アートプラザ )
 2019「THE SIX COGNITION ~ Affirmative ~」(Art space kimura ASK? B1)
-            `,
+
+2021 第15回アートプラザ大賞 入選
+
+銀座Whitestone galleryを活動拠点に若手作家として絵画・映像インスタレーションの展示、絵画の販売を行っている。その他Whitestone gallery、レイワセダ株式会社にてティザー動画のディレクション、編集を担当。2022/8/23-28より谷中hacoギャラリーにて個展を開催予定。
+`,
             links: [
                 {
                     text: "Portfolio",
-                    url: "https://hyasssy.tokyo/"
+                    url: "https://ariha12245.wixsite.com/portforio"
                 },
                 {
-                    text: "Twitter: @hyasssy",
-                    url: "https://twitter.com/hyasssy"
+                    text: "Twitter",
+                    url: "https://twitter.com/Ariha_IMAMIYA"
                 }
             ]
         }
@@ -46,21 +51,59 @@ const _m2data = [
             name_ja: "桒原 幹治",
             name_en: "Kanji Kuwahara",
             work1_img: "./images/artistsinfo/m2/kuwahara/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《objets》（2022）",
             work2_img: "./images/artistsinfo/m2/kuwahara/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《線》（2021）",
             work3_img: "",
             work3_caption: "",
             info: `プロフィール
-hogehogheoge`,
+1998年宮崎県出身。リズムの概念を新たに捉え直すべく、演奏者としての経験やリズム論の研究をもとに、映像・平面・サウンドインスタレーション・パフォーマンスなど多様なメディアでの制作を行う。東京藝術大学音楽学部音楽環境創造科卒業。
+
+展示歴
+2022 個展「身を以って」（Room_412）
+2022「第2回みやざき総合美術展」（宮崎県立美術館）
+2021「環ジョウ交さ点」（佐賀大学美術館）
+2021「CAF賞2021 入選作品展覧会」（代官山ヒルサイドフォーラム）
+2021「NIME2021」（上海ニューヨーク大学/オンライン）
+2021 個展「リズムのための試論」（Room_412）
+2020「寝床 a.k.a. 混淆する幻想半径」（shibuya-san）
+2020「バトルフィールド2」（Room_412）
+2019「ICSAF2019」（尚美学園大学）
+2019「バトルフィールド」（Room_412）
+2018「あの子のこと」（KISYURYURI THEATER）
+
+活動歴
+2022「音沙汰 vol.2」（BUoY）
+2021「発光する音楽」（いい貸家）
+2021 カブトムシ 1st Album『色即ラグーン』
+2021 カブトムシ「Flower」
+2021 かさねぎリストバンド「水属性」（東京芸術劇場）
+2021 LA SEÑAS「FREE RAVE vol.1」（稲毛海浜公園野外音楽堂）
+2019「SLEEP 1」（落合soup）
+
+受賞歴
+2022 IAG AWARDS 2022 入選
+2022 第2回みやざき総合美術展 自由表現部門 準特選
+2021 CAF賞2021 入選
+2021 東京藝術大学音楽学部 同声会賞
+2020 東京藝術大学 安宅賞
+
+助成
+2022 クマ財団クリエイター支援奨学金
+2021 三菱商事アート・ゲート・プログラム
+2020 武藤舞音楽環境創造教育研究助成金
+
+刊行物
+2021 『リズムのための試論』自主出版
+`,
             links: [
                 {
                     text: "Portfolio",
-                    url: "https://hyasssy.tokyo/"
+                    url: "https://www.acpmk.net/"
                 },
                 {
-                    text: "Twitter: @hyasssy",
-                    url: "https://twitter.com/hyasssy"
+                    text: "Twitter: @acpmk",
+                    url: "https://twitter.com/acpmk"
                 }
             ]
         }
@@ -74,21 +117,41 @@ hogehogheoge`,
             name_ja: "佐藤 瞭太郎",
             name_en: "Ryotaro Sato",
             work1_img: "./images/artistsinfo/m2/sato/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《All Night》（2022）",
             work2_img: "./images/artistsinfo/m2/sato/2.jpg",
-            work2_caption: "hogehoge",
-            work3_img: "./images/artistsinfo/m2/sato/3.jpg",
-            work3_caption: "hogehoge",
+            work2_caption: "《手腕振動》（2021）",
+            work3_img: "",
+            work3_caption: "",
             info: `プロフィール
-hogehogheoge`,
+過去の文学や映画、インターネット上を流通するデータを引用/サンプリングすることによって、今日における事物の移動と変形について思考する映像作品を中心に制作している。
+
+展示歴
+2022 個展「Exercise For Drifting」 (thecaコ本や honkbooks)
+2022「第27回学生CGコンテスト　オンライン・ノミネート作品展」(オンライン)
+2021「第26回学生CGコンテスト　オンライン・ノミネート作品展」(オンライン)
+2021「ANTEROOM Transmission vol.1 - 変容する社会の肖像」(HOTEL ANTEROOM KYOTO)
+2020「CAIR2020」(筑波大学総合交流会館)
+2020「FACE」(6A208)
+2020「CAF賞2020」(代官山ヒルサイドフォーラム)
+2018「girl」(Cafe4)
+
+受賞歴
+2022　ASK?映像祭コンペティション　入選
+2022　第27回学生CGコンテスト 審査員賞
+2022　やまなしメディア芸術アワード 入選
+2021　第26回学生CGコンテスト 評価員賞
+2020　 CAF賞2020 入選
+
+
+`,
             links: [
                 {
                     text: "Portfolio",
-                    url: "https://hyasssy.tokyo/"
+                    url: "ryotarosato.com"
                 },
                 {
-                    text: "Twitter: @hyasssy",
-                    url: "https://twitter.com/hyasssy"
+                    text: "Twitter: @tansansui62560",
+                    url: "https://twitter.com/tansansui62560"
                 }
             ]
         }
@@ -100,23 +163,27 @@ hogehogheoge`,
         lab: "畠山研究室",
         data: {
             name_ja: "田中くるみ",
-            name_en: "Ryotaro Sato",
+            name_en: "Kurumi Tanaka",
             work1_img: "./images/artistsinfo/m2/tanaka/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "",
             work2_img: "./images/artistsinfo/m2/tanaka/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "",
             work3_img: "./images/artistsinfo/m2/tanaka/3.jpg",
-            work3_caption: "hogehoge",
+            work3_caption: "《土のポートレイツ》（2022）",
             info: `プロフィール
-hogehogheoge`,
+岩手県生まれ。ある場所の持つ歴史的時間、物理的空間、それらからこぼれ落ちるものついて、映像・言語表現を用いて考察する。リサーチやフィールドワークなど、自身の体を通した経験をもとに制作を行う。千葉大学工学部建築学科卒。
+
+刊行物
+第34回文学フリマ東京『中華義荘日記』発行
+`,
             links: [
                 {
-                    text: "Portfolio",
-                    url: "https://hyasssy.tokyo/"
+                    text: "Instagram",
+                    url: "https://www.instagram.com/mol__3"
                 },
                 {
-                    text: "Twitter: @hyasssy",
-                    url: "https://twitter.com/hyasssy"
+                    text: "Twitter",
+                    url: "https://twitter.com/mol6023"
                 }
             ]
         }
@@ -130,11 +197,11 @@ hogehogheoge`,
             name_ja: "林 裕人",
             name_en: "Yuto Hayashi",
             work1_img: "./images/artistsinfo/m2/hayashi/1.png",
-            work1_caption: "hogehoge",
+            work1_caption: "",
             work2_img: "./images/artistsinfo/m2/hayashi/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "",
             work3_img: "./images/artistsinfo/m2/hayashi/3.jpg",
-            work3_caption: "hogehoge",
+            work3_caption: "《Can I see You now?》（2021）",
             info: `プロフィール
 情報と身体が一体化する未来のプロダクトのプロトタイピングを行なっている。
 在学中にスタートアップでのPdMを経験。アート・文化思想的なバックグラウンドと、先端技術を活用したプロダクトの社会実装の経験から、多角的な未来の思考を試みる。
