6411bc5ed2b919f5dc9589fe7257ef11cbfb684f
Yuto Hayashi <hyasssy@gmail.com>
2022-07-17 11:24:15 +0900

    変更を反映

--- js/artistsinfo.js
+++ js/artistsinfo.js
@@ -2,12 +2,29 @@
 const _m2data = [
     {
         base_id: "imamiya",
-        thumb_img_path: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
-        name: "今宮有葉",
-        lab: "畠山研究室",
+        thumb_img_path: "./images/artistsinfo/m2/thumbnails/imamiya.jpg",
+        name: "今宮 有葉",
+        lab: "桐山研究室",
         data: {
-            name_ja: "今宮有葉",
+            name_ja: "今宮 有葉",
             name_en: "Ariha Imamiya",
+            work1_img: "./images/artistsinfo/m2/imamiya/1.jpg",
+            work1_caption: "hogehoge",
+            work2_img: "./images/artistsinfo/m2/imamiya/2.jpg",
+            work2_caption: "hogehoge",
+            work3_img: "./images/artistsinfo/m2/imamiya/nagata.jpg",
+            work3_caption: "hogehoge",
+            info: "hogehogheoge"
+        }
+    },
+    {
+        base_id: "kuwahara",
+        thumb_img_path: "./images/artistsinfo/m2/thumbnails/kuwahara.jpg",
+        name: "桒原 幹治",
+        lab: "服部研究室",
+        data: {
+            name_ja: "桒原 幹治",
+            name_en: "Kanji Kuwahara",
             work1_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
             work1_caption: "hogehoge",
             work2_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
@@ -18,13 +35,13 @@ const _m2data = [
         }
     },
     {
-        base_id: "nagata2",
-        thumb_img_path: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
-        name: "永田",
+        base_id: "sato",
+        thumb_img_path: "./images/artistsinfo/m2/thumbnails/sato.jpg",
+        name: "佐藤 瞭太郎",
         lab: "畠山研究室",
         data: {
-            name_ja: "永田",
-            name_en: "NAGATA",
+            name_ja: "佐藤 瞭太郎",
+            name_en: "Ryotaro Sato",
             work1_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
             work1_caption: "hogehoge",
             work2_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
@@ -35,18 +52,35 @@ const _m2data = [
         }
     },
     {
-        base_id: "nagata3",
-        thumb_img_path: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
-        name: "永田",
+        base_id: "tanaka",
+        thumb_img_path: "./images/artistsinfo/m2/thumbnails/tanaka.jpg",
+        name: "田中くるみ",
         lab: "畠山研究室",
         data: {
-            name_ja: "永田",
-            name_en: "NAGATA3",
+            name_ja: "田中くるみ",
+            name_en: "Ryotaro Sato",
             work1_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
             work1_caption: "hogehoge",
             work2_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
             work2_caption: "hogehoge",
             work3_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
+            work3_caption: "hogehoge",
+            info: "hogehogheoge"
+        }
+    },
+    {
+        base_id: "hayashi",
+        thumb_img_path: "./images/artistsinfo/m2/thumbnails/hayashi.jpg",
+        name: "林 裕人",
+        lab: "桐山研究室",
+        data: {
+            name_ja: "林 裕人",
+            name_en: "Yuto Hayashi",
+            work1_img: "./images/artistsinfo/m2/hayashi/1.png",
+            work1_caption: "hogehoge",
+            work2_img: "./images/artistsinfo/m2/hayashi/2.jpg",
+            work2_caption: "hogehoge",
+            work3_img: "./images/artistsinfo/m2/hayashi/3.jpg",
             work3_caption: "hogehoge",
             info: "hogehogheoge"
         }
@@ -54,18 +88,18 @@ const _m2data = [
 ]
 const _m1data = [
     {
-        base_id: "ito",
-        thumb_img_path: "./images/artistsinfo/m1/thumbnails/ito.jpg",
-        name: "伊藤",
+        base_id: "itok",
+        thumb_img_path: "./images/artistsinfo/m1/thumbnails/itok.jpg",
+        name: "伊藤 琴音",
         lab: "畠山研究室",
         data: {
-            name_ja: "伊藤",
-            name_en: "Ito",
-            work1_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
+            name_ja: "伊藤 琴音",
+            name_en: "Ito Kotone",
+            work1_img: "./images/artistsinfo/m1/itok/1.jpg",
             work1_caption: "hogehoge",
-            work2_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
+            work2_img: "./images/artistsinfo/m1/itok/2.jpg",
             work2_caption: "hogehoge",
-            work3_img: "./images/artistsinfo/m2/thumbnails/nagata.jpg",
+            work3_img: "./images/artistsinfo/m1/itok/3.jpg",
             work3_caption: "hogehoge",
             info: "hogehogheoge"
         }
