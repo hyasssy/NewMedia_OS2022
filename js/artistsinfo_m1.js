be3b2a4b24767527b5101195f81fe2a69a911129
HaruSF <unitharu@gmail.com>
2022-07-17 14:15:24 +0900

    Update artistsinfo_m1.js

--- js/artistsinfo_m1.js
+++ js/artistsinfo_m1.js
@@ -7,12 +7,18 @@ const _m1data = [
             name_ja: "安藤 美由",
             name_en: "Miyu Ando",
             work1_img: "./images/artistsinfo/m1/ando/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《影の記録》（2022）",
             work2_img: "./images/artistsinfo/m1/ando/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《鶏皮飼育日記》（2019）",
             work3_img: "./images/artistsinfo/m1/ando/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《水槽に入れた鶏皮》（2022）",
+            info: `プロフィール
+            武蔵野美術大学造形学部映像学科卒業。
+            触る事で対象についての知見を得、その記録の集積を元に制作を行う。
+            
+            展示歴
+            2022「令和三年度 武蔵野美術大学 卒業・修了制作展」（武蔵野美術大学）
+            `
         }
     },
     {
@@ -23,12 +29,27 @@ const _m1data = [
             name_ja: "和泉 はるな",
             name_en: "Haruna Izumi",
             work1_img: "./images/artistsinfo/m1/izumi/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《織り語り》（2022）",
             work2_img: "./images/artistsinfo/m1/izumi/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《これはネコではない》（2019）",
             work3_img: "./images/artistsinfo/m1/izumi/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《INVISIBLE》（2019）",
+            info: `プロフィール
+            1995年宮城県石巻市生まれ。
+            他者との論理的な共通認識としての作用を期待し、言語にアイデアを得て制作を行う。
+            多摩美術大学美術学部グラフィックデザイン学科卒業。
+            
+            展示歴
+            2021「多摩美術大学グラフィックデザイン学科卒業制作展2021」（恵比寿ガーデンプレイス）
+            2020 「多摩美術大学美術学部卒業制作展・大学院修了制作展B」（多摩美術大学）
+            2019「 多摩美術大学地震ポスター支援プロジェクト・イラストレーションポスター展」(多摩美術大学)
+            2017「PORTFOLIO 2017」（6次元）
+            2016「JAGDA学生グランプリ2016」（東京ミッドタウン・デザインハブ）
+
+            受賞歴
+            2017「東京TDC賞2017」入選
+            2016「JAGDA学生グランプリ」入選
+            `
         }
     },
     {
@@ -39,12 +60,19 @@ const _m1data = [
             name_ja: "伊藤 琴音",
             name_en: "Kotone Ito",
             work1_img: "./images/artistsinfo/m1/itok/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《流水》（2022）",
             work2_img: "./images/artistsinfo/m1/itok/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《あわいをきく》（2021）",
             work3_img: "./images/artistsinfo/m1/itok/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《Selection》（2020）",
+            info: `プロフィール
+            1999年東京都生まれ。
+            2022年東京藝術大学音楽学部音楽環境創造科卒業。
+            聴覚を主軸とした知覚についての思索や制作を行う。
+            
+            展示歴
+            2022「東京藝術大学音楽環境創造科卒業制作・論文発表会 2022」（東京藝術大学千住キャンパス）
+            `
         }
     },
     {
@@ -55,12 +83,24 @@ const _m1data = [
             name_ja: "伊藤 道史",
             name_en: "Michibumi Ito",
             work1_img: "./images/artistsinfo/m1/itom/1.png",
-            work1_caption: "hogehoge",
+            work1_caption: "《パレーシアの地平》（2022）",
             work2_img: "./images/artistsinfo/m1/itom/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《クロールする蛹のためのレクチャー》（2021）",
             work3_img: "./images/artistsinfo/m1/itom/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《 皮膚祭 / XipeTotec Reality》（2021）",
+            info: `プロフィール
+            バーチャルリアリティを中心に用いて制作をしてきた。
+            病理や宗教などある人々にとって非現実的(バーチャル/フィクショナル)なイメージを扱いながら、今私たちがいる時空間とは別にありえた世界について考えている。
+            
+            展示歴
+            –個人
+            2021「個展-クロールする蛹のためのレクチャー」 （TIME MACHINE）
+            –グループ・企画
+            2022「NEWVIEW FEST 2021-皮膚祭 / XipeTotec Reality」（渋谷PARCO 4F / バーチャル会場）
+            2021「IAMAS_Open_house 2021」（オンライン）
+            2021「第9回 VROD」（FinGATE KAYABA）
+            2021「S.C.L World #01」（渋谷クオーツギャラリー）
+            `
         }
     },
     {
@@ -71,12 +111,25 @@ const _m1data = [
             name_ja: "江口 智之",
             name_en: "Tomoyuki Eguchi",
             work1_img: "./images/artistsinfo/m1/eguchi/1.jpeg",
-            work1_caption: "hogehoge",
+            work1_caption: "《Following》（2022）",
             work2_img: "./images/artistsinfo/m1/eguchi/2.jpeg",
-            work2_caption: "hogehoge",
+            work2_caption: "《Nastagio degli Onesti》（2022）",
             work3_img: "./images/artistsinfo/m1/eguchi/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《PERFECT CRIME》（2021）",
+            info: `プロフィール            
+            1998年生まれ。
+            2022年、多摩美術大学絵画学科油画専攻卒業。
+            美術館をバックグラウンドに持つ意識から美術の特権性及び、公益性についての研究、展示を行う。
+            
+            展示歴
+            2022「ストーンテープ~見たら呪われる展示~」（PARA / 住所非公開）
+            2021「Navigated Locomotion」（White Gallery / 東京・千駄木）
+            2021「獸(第 0 章/交叉時点)」（BUoY / 東京・北千住）
+            2020「個展 -スピーカー-」（rusu / 東京・目黒）
+
+            活動歴
+            2020「ウォーターフォールを追いかけて」映像制作（早稲田小劇場どらま館招聘公演・オンライン上演）
+            `
         }
     },
     {
@@ -87,12 +140,32 @@ const _m1data = [
             name_ja: "小田 陽菜乃",
             name_en: "Hinano Oda",
             work1_img: "./images/artistsinfo/m1/oda/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《檸檬/変身》（2022）",
             work2_img: "./images/artistsinfo/m1/oda/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《食い入るように》（2021）",
             work3_img: "./images/artistsinfo/m1/oda/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《丁寧な錯覚》(2020)",
+            info: `プロフィール            
+            1997年神奈川県生まれ。
+            2022年金沢美術工芸大学美術工芸学部美術科彫刻専攻卒業。
+            作品を多視点から鑑賞する際の身体感覚を通して、見ているものの曖昧さについて考え、制作を行う。
+
+            展示歴
+            2021「ストレンジャーによろしく」（箔一ビル、石川）
+            2021「金沢彫刻祭2019」（じょーの箱、石川）
+            2019「oterart2019」（崇禅寺、石川）
+            2019「東京インディペンデント」（東京藝術大学美術館　陳列館、東京）
+            2019「二人展　胎児の夢」（DESIGN FESTA GALLERY WEST1-D、
+            東京）
+            2018「oterart2018」（全性寺、石川）
+
+            活動歴
+            金沢美術工芸大学アートプロジェクトチーム「スズプロ」で活動。
+            2021「奥能登国際芸術祭2020+」（旧八木家、石川）
+
+            助成
+            2021　三菱商事アート・ゲート・プログラム
+            `
         }
     },
     {
@@ -101,14 +174,26 @@ const _m1data = [
         name: "倉知 朋之介",
         data: {
             name_ja: "倉知 朋之介",
-            name_en: "Tomonosuke Krachi",
+            name_en: "Tomonosuke Kurachi",
             work1_img: "./images/artistsinfo/m1/kurachi/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《hogehoALIEN BOOK》（2022）",
             work2_img: "./images/artistsinfo/m1/kurachi/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《ムシ図鑑》（2022）",
             work3_img: "./images/artistsinfo/m1/kurachi/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《チョコチップクッキー＆ミルク》（2022）",
+            info: `プロフィール            
+            1997年愛知県生まれ。
+            2020年京都造形芸術大学（現 京都芸術大学）情報デザイン学科卒業。
+            日常生活のなかにある「可笑しさ」を帯びた存在や状況に焦点を当て、映像作品を中心に制作を行う。
+
+            展示歴
+            2022「惑星ザムザ」（小高製本工業株式会社跡地、東京）
+            2022「ARTISTS’FAIR KYOTO 2022」（京都新聞ビル 地下1階、京都）
+            2022「NITO09」（アート／空家 二人、東京）
+
+            受賞歴
+            2020「2019年度京都造形芸術大学作品展」優秀賞
+            `
         }
     },
     {
@@ -119,12 +204,19 @@ const _m1data = [
             name_ja: "是永 悠之介",
             name_en: "Yunosuke Korenaga",
             work1_img: "./images/artistsinfo/m1/korenaga/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《Lights》（2022）",
             work2_img: "./images/artistsinfo/m1/korenaga/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《空ける、ユーリカ》（2022）",
             work3_img: "./images/artistsinfo/m1/korenaga/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《Look out of the window》（2020）",
+            info: `プロフィール            
+            2000年大分県生まれ。
+            東京造形大学造形学部美術学科絵画専攻卒業。
+            インスタレーション、映像を中心に制作。
+
+            展示歴
+            2022「ZOKEI展」（東京造形大学）
+            `
         }
     },
     {
@@ -135,12 +227,15 @@ const _m1data = [
             name_ja: "齋藤 千春",
             name_en: "Chiharu Saito",
             work1_img: "./images/artistsinfo/m1/saito/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《ラジオドラマ》（2022）",
             work2_img: "./images/artistsinfo/m1/saito/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《Somewhere》（2021）",
             //work3_img: "./images/artistsinfo/m1/saito/3.jpg",
             //work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            info:`プロフィール            
+            2000年生まれ。
+            日本大学芸術学部写真学科卒業。
+            `
         }
     },
     {
@@ -151,12 +246,27 @@ const _m1data = [
             name_ja: "鈴木 創大",
             name_en: "Sota Suzuki",
             work1_img: "./images/artistsinfo/m1/suzuki/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《オノマトペ》（2022）",
             work2_img: "./images/artistsinfo/m1/suzuki/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《Delicious water》（2022）",
             work3_img: "./images/artistsinfo/m1/suzuki/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《139.72646653207784° 35.665113849127884°》（2021）",
+            info: `プロフィール
+            1999年生まれ。
+            多摩美術大学美術学部絵画学科油画専攻2022年卒業。
+            近年は『ペラい場所性』についてをテーマにインスタレーションや映像などを中心に制作をしている。
+            またVJ・映像作家としても活動している。          
+
+            展示歴
+            2022「多摩美術大学 卒業制作展・大学院修了制作展」
+            2022「第45回 東京五美術大学連合卒業・修了制作展」
+            2021 「個展-Delicious Water-」(TAU Painting East Wing 402)
+            2020「フラット３階建て」（プライベイト、東京）
+            2019 「acousticlubvol.4」（千代田区 内幸町ホール）
+
+            受賞歴
+            2022 「多摩美術大学 卒業制作展」優秀賞
+            `
         }
     },
     {
@@ -167,12 +277,25 @@ const _m1data = [
             name_ja: "チャンヤ･スィウランポン",
             name_en: "Chanya Sriuranpong",
             work1_img: "./images/artistsinfo/m1/chanya/1.png",
-            work1_caption: "hogehoge",
+            work1_caption: "《GOOD MORNING》（2022）",
             work2_img: "./images/artistsinfo/m1/chanya/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《GHOSTLY NEIGHBOUR》（2019）",
             work3_img: "./images/artistsinfo/m1/chanya/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            work3_caption: "《REALITY ESCAPE CLOAK》（2018）",
+            info: `プロフィール
+            タイ、バンコク出身。
+            チュラーロンコーン大学コミュニケーションデザイン学科卒業。
+            
+            展示歴
+            -個人
+            2019 「CommDe Creative Walk 2019 'Commbo'」(卒業・修了制作展)
+            -グループ・企画
+            2017 「SILENT SPEAKER」(RATCHADAMNONE CONTENPORARY ART CENTRE (RCAC))
+            2017 「INTERNATIONAL COLLABORATION OF CHULALONGKORN UNIVERSITY AND SHIH CHIEN UNIVERSITY TAIPEI」
+            
+            受賞歴
+            2018「ART GAMES – AN INTERNATIONAL GAME JAM NETWORK 」(Top 3 Finalist)
+            `
         }
     },
     {
@@ -183,12 +306,36 @@ const _m1data = [
             name_ja: "本間 悠暉",
             name_en: "Yuki Homma",
             work1_img: "./images/artistsinfo/m1/homma/1.jpg",
-            work1_caption: "hogehoge",
+            work1_caption: "《Para-Vision》（2022）",
             work2_img: "./images/artistsinfo/m1/homma/2.jpg",
-            work2_caption: "hogehoge",
+            work2_caption: "《Project GIBSON（グループ・企画 2021）》",
             //work3_img: "./images/artistsinfo/m1/homma/3.jpg",
             //work3_caption: "hogehoge",
-            info: "hogehogheoge"
+            info: `プロフィール
+            神奈川県横浜市出身。
+            東京大学経済学部経済学科卒業。
+            2018年より、MESON, incにてAR/VR技術を活用した体験の企画・研究活動に参画。
+            都市の記憶のアーカイブや、メディアを介した個人像に着想を得て制作を行う。
+
+            展示歴
+            -グループ・企画
+            2021「SIGGRAPH ASIA 2021」（東京国際フォーラム）
+            2020 「未来の学校祭」（東京ミッドタウン）
+            2020 「CES2020」（ラスベガス・コンベンションセンター）
+
+            活動歴
+            2021 国土交通省Project Plateau 民間サービス実証調査参画
+            2020 ノイタミナ系列『富豪刑事』ARデザインアドバイザー参画
+            2018 MESON, inc にディレクターとして参画し企業とのXR技術を活用した研究を行う
+          
+            受賞歴
+            2021年 「SIGGRAPH ASIA2021 XR」論文掲載
+            2019年 East Gateハッカソン優勝
+
+            刊行物
+            2021 論文共著『GIBSON: AR/VR synchronized city walking system』（SIGGRAPH ASIA 2021 XR）
+            2021 プロダクト掲載『WIRED JAPAN VOL.41』
+            `
         }
     },
 
