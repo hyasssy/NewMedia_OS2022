2185226701081090aa72d9ffb11037c9af02d2d4
Yuto Hayashi <hyasssy@gmail.com>
2022-07-17 11:37:31 +0900

    消し忘れ反映

--- js/artistsinfo_m2.js
+++ js/artistsinfo_m2.js
@@ -93,22 +93,3 @@ const _m2data = [
         }
     }
 ]
-const _m1data = [
-    {
-        base_id: "itok",
-        thumb_img_path: "./images/artistsinfo/m1/thumbnails/itok.jpg",
-        name: "伊藤 琴音",
-        lab: "畠山研究室",
-        data: {
-            name_ja: "伊藤 琴音",
-            name_en: "Ito Kotone",
-            work1_img: "./images/artistsinfo/m1/itok/1.jpg",
-            work1_caption: "hogehoge",
-            work2_img: "./images/artistsinfo/m1/itok/2.jpg",
-            work2_caption: "hogehoge",
-            work3_img: "./images/artistsinfo/m1/itok/3.jpg",
-            work3_caption: "hogehoge",
-            info: "hogehogheoge"
-        }
-    }
-]
